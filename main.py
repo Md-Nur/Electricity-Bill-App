e0cc1dd0eb9927eff883540b074ae9e5906de333
Nur <md.nurealamsiddiquee2004@gmail.com>
2022-03-04 22:14:27 +0600

    try to add photo

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 from tkinter import *
-
+# from PIL import ImageTk, Image
+NAME = "Electrical Bill"
 root = Tk()
+root.geometry("1000x500")
+root.minsize(1000, 500)
+root.title(NAME)
+title = Label(root, text=NAME, font=("Arial Bold", 30))
+title.pack()
+photo = PhotoImage('xyz.png')
+l = Label(image = photo)
+l.pack()
 
+root.mainloop()
