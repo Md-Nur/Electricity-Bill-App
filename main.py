d77b944b2a503c522e9ad20484a2e38d2894faee
Nur <md.nurealamsiddiquee2004@gmail.com>
2022-03-17 21:05:29 +0600

    complete mysql part and strat for pdf

--- main.py
+++ main.py
@@ -12,15 +12,33 @@ def preData(roomNo):
     cpmon = months[datetime.now().month-3]
     conn = mysql.connector.connect(host="localhost", user="root", password="khelahobe", database=Database)
     
-    room_insert = conn.cursor()
     room_collect = conn.cursor()
     room_collect.execute("SELECT * FROM `test_elec_bill`.`room` WHERE `month`=%s AND `year`=%s AND `room_no` = %s;", (cpmon, datetime.now().year,roomNo))
     
     pRoomData = room_collect.fetchall()[0]
     print(pRoomData)
-    
+    conn.commit()
+    conn.close()
     return pRoomData
     # room_insert.execute("INSERT INTO `test_elec_bill`.`room` ( `room_no`, `year`, `month`, `name`, `rate`, `room_fare`, `room_unit`, `room_advance`) VALUES(%s, %s, %s, %s, %s, %s, %s, %s);" ( '101', '2022', month.get(), 'শরীফ', '5.45', '7500', '34556', '0'))
+
+
+def dbRoom(room_net_units):
+    global pmon ,room_101, room_102, room_103, room_201, room_202, room_203, room_401, room_402, room_403, room_404,months
+    
+    room_list = [room_101, room_102, room_103, room_201, room_202, room_203, room_401, room_402, room_403, room_404]
+    
+    conn = mysql.connector.connect(host="localhost", user="root", password="khelahobe", database=Database)
+    room_insert = conn.cursor()
+    
+    for rnu,rl in zip(room_net_units,room_list):
+        inpt = (int(rl.room_no[:3]),year.get() , month.get(), rl.room_name, rate.get(), rl.room_fare.get(), rnu, rl.room_advance.get())
+        room_insert.execute("INSERT INTO `test_elec_bill`.`room` ( `room_no`, `year`, `month`, `name`, `rate`, `room_fare`, `room_unit`, `room_advance`) VALUES(%s, %s, %s, %s, %s, %s, %s, %s);",inpt )
+    # INSERT INTO `test_elec_bill`.`room` (`room_no`, `year`, `month`, `name`, `rate`, `room_fare`, `room_unit`, `room_advance`) VALUES ('101', '2021', 'December', 'Shorif', '5.45', '7500', '3400', '0');
+    print("Data Inserted")
+    conn.commit()
+    conn.close()
+
 
 def dataBase():
     global pmon ,room_101, room_102, room_103, room_201, room_202, room_203, room_401, room_402, room_403, room_404,months
@@ -30,10 +48,7 @@ def dataBase():
     room_units = [room.room_unit.get() for room in room_list]
     room_units.extend([month.get(), year.get()])
     print(room_units)
-    pyear = year.get()
-    if pmon == "December":
-        pyear -= 1
-
+    
     conn = mysql.connector.connect(host="localhost", user="root", password="khelahobe", database=Database)
     cursor = conn.cursor()
 
@@ -41,7 +56,10 @@ def dataBase():
 
     collect = conn.cursor()
     collect.execute("SELECT * FROM `test_elec_bill`.`miter` WHERE `month`=%s AND `year`=%s;", (pmon, year.get()))
-    pdata = collect.fetchall()[0][1:11]
+    try:
+        pdata = collect.fetchall()[0][1:11]
+    except IndexError:
+        pdata = pmon
     print(pdata)
     
     room_net_units = []
@@ -50,9 +68,9 @@ def dataBase():
     room_net_units[2] = room_net_units[2] - room_net_units[1]
     room_net_units[5] = room_net_units[5] - room_net_units[4] - room_net_units[3]
     print(room_net_units)
-    
     conn.commit()
     conn.close()
+    dbRoom(room_net_units)
     
     
 def displayUnit():
@@ -68,7 +86,7 @@ class room():
         self.room_name = room_name
         
         self.room_no = room_no
-        self.room_label = Label(tableFrame, text=f"{self.room_name} ({room_no}) :", font=FONT)
+        self.room_label = Label(tableFrame, text=f"{self.room_name} ({self.room_no}) :", font=FONT)
         self.room_label.grid(row=row, column=0,padx=5,pady=5)
         
         self.room_unit = Entry(tableFrame, font=FONT)
@@ -114,25 +132,25 @@ if __name__ == '__main__':
     row+=1
     
     pr101 = preData(101)
-    room_101 = room("নিচ তলার সাব ১ (টিনশেড) নং",room_name=pr101[4],room_fare=pr101[-3])
+    room_101 = room("101 নিচ তলার সাব ১ (টিনশেড) নং",room_name=pr101[4],room_fare=pr101[-3])
     pr102 = preData(102)
-    room_102 = room("নিচ তলার সাব ২ নং",room_name=pr102[4],room_fare=pr102[-3])
+    room_102 = room("102 নিচ তলার সাব ২ নং",room_name=pr102[4],room_fare=pr102[-3])
     pr103 = preData(103)
-    room_103 = room("নিচ তলার মেইন",room_name=pr103[4],room_fare=pr103[-3])
+    room_103 = room("103 নিচ তলার মেইন",room_name=pr103[4],room_fare=pr103[-3])
     pr201 = preData(201)
-    room_201 = room("২ তলার সাব ১ নং",room_name=pr201[4],room_fare=pr201[-3])
-    pr202 = preData(201)
-    room_202 = room("২ তলার সাব ২ নং",room_name=pr202[4],room_fare=pr202[-3])
-    pr203 = preData(202)
-    room_203 = room("২ তলার মেইন",room_name=pr203[4],room_fare=pr203[-3])
-    pr401 = preData(203)
-    room_401 = room("৪ তলার সাব ১ নং",room_name=pr401[4],room_fare=pr401[-3])
-    pr402 = preData(401)
-    room_402 = room("৪ তলার সাব ২ নং",room_name=pr402[4],room_fare=pr402[-3])
+    room_201 = room("201 ২ তলার সাব ১ নং",room_name=pr201[4],room_fare=pr201[-3])
+    pr202 = preData(202)
+    room_202 = room("202 ২ তলার সাব ২ নং",room_name=pr202[4],room_fare=pr202[-3])
+    pr203 = preData(203)
+    room_203 = room("203 ২ তলার মেইন",room_name=pr203[4],room_fare=pr203[-3])
+    pr401 = preData(401)
+    room_401 = room("401 ৪ তলার সাব ১ নং",room_name=pr401[4],room_fare=pr401[-3])
+    pr402 = preData(402)
+    room_402 = room("402 ৪ তলার সাব ২ নং",room_name=pr402[4],room_fare=pr402[-3])
     pr403 = preData(403)
-    room_403 = room("৪ তলার সাব ৩ নং",room_name=pr403[4],room_fare=pr403[-3])
+    room_403 = room("403 ৪ তলার সাব ৩ নং",room_name=pr403[4],room_fare=pr403[-3])
     pr404 = preData(404)
-    room_404 = room("৪ তলার সাব ৪ নং",room_name=pr404[4],room_fare=pr404[-3])
+    room_404 = room("404 ৪ তলার সাব ৪ নং",room_name=pr404[4],room_fare=pr404[-3])
     
     tableFrame2 =Frame(root,padx=10,pady=10)
     tableFrame2.pack()
